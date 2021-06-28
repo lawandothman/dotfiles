3d53d75304c2b1f8542be71785e19d7639a92f18
Lawand Othman <lwnd@pm.me>
2021-06-28 13:53:52 +0100

    clone dotfiles with the install script

--- install.sh
+++ install.sh
@@ -32,3 +32,13 @@ source ~/.zshrc
 nvm install node
 nvm use node
 npm install -g yarn
+echo 🚀 Cloning your dotfiles from GitHub 🚀
+alias config='/opt/homebrew/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo ".cfg" >> .gitignore
+git clone --bare https://github.com/lawandothman/dotfiles.git $HOME/.cfg
+alias config='/opt/homebrew/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+config checkout
+config config --local status.showUntrackedFiles no
