159f2532fa2e85f593a3e5a74698cec676bd58a2
Lawand Othman <lwnd@pm.me>
2026-08-08 14:58:35 +0100

    nvim: replace .luarc.json with lazydev.nvim

--- home/.config/nvim/lsp/lua_ls.lua
+++ home/.config/nvim/lsp/lua_ls.lua
@@ -3,13 +3,7 @@ return {
   settings = {
     Lua = {
       runtime = { version = 'LuaJIT' },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          '${3rd}/luv/library',
-          unpack(vim.api.nvim_get_runtime_file('', true)),
-        },
-      },
+      workspace = { checkThirdParty = false },
       completion = {
         callSnippet = 'Replace',
       },
