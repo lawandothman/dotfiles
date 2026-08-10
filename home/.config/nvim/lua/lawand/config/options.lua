478fb586ac4a6c288bf79057e87cf64865cd3390
Lawand Othman <lwnd@pm.me>
2026-08-10 13:36:58 +0100

    nvim: stop rendering trailing whitespace glyphs

--- home/.config/nvim/lua/lawand/config/options.lua
+++ home/.config/nvim/lua/lawand/config/options.lua
@@ -59,12 +59,6 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
--- Sets how neovim will display certain whitespace in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
-
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
